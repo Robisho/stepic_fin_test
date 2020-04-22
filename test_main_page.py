8d5d646c60e86670ba37da97e8ffedc69ecd3ba2
Robisho <bigsten@list.ru>
2020-04-22 12:12:04 +0800

    create file

---
+++ test_main_page.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+# pytest -v --tb=line --language=en test_main_page.py
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link)
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
