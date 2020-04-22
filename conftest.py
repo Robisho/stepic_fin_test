b9e5720a064735a6a493444cfffec5249df10497
Robisho <bigsten@list.ru>
2020-04-22 10:46:32 +0800

    create conftest

---
+++ conftest.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name',
+                    action='store',
+                    default='chrome',
+                    help='Выберите браузер: chrome или firefox')
+    parser.addoption('--language',
+                    action='store',
+                    default='en',
+                    help='Выберите язык: ru, en-gb, ..., (etc)')
+
+
+@pytest.fixture(scope='function')  # браузер запускается на каждый id
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    user_language = request.config.getoption('language')
+    browser = None
+    if browser_name == 'chrome':
+        print('\nStart chrome browser for test......')
+        options = Options()
+        options.add_experimental_option(
+            'prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)  # работает раз через два
+    elif browser_name == 'firefox':
+        print('\nStart firefox browser for test......')
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference('intl.accept_language', user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError(
+            '--browser_name должно быть chrome или firefox')
+    yield browser
+    print('\nQuit browser....')
+    time.sleep(5)
+    browser.quit()
