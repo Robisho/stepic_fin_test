b84acda2df93aa622f3f18305a9f4a64b89a935d
Robisho <bigsten@list.ru>
2020-05-17 21:51:39 +0800

    add class ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,12 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#id_login-username')
     REGISTER_FORM = (By.CSS_SELECTOR, '#id_registration-email')
+
+class ProductPageLocators():
+    BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+
+    # BASE_BOOKNAME = (By.CSS_SELECTOR, '.product_main h1')  # +
+    BASE_BOOKNAME = (By.XPATH, '//*[@id="content_inner"]//h1')  # +
+
+    # BASE_PRICE = (By.CSS_SELECTOR, '.product_main .price_color')  # +
+    BASE_PRICE = (By.XPATH, '//*[@id="content_inner"]//p[1]')  # +
