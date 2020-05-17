c7c144e3e12352750fec6e24e01861a7d9360410
Robisho <bigsten@list.ru>
2020-05-17 21:56:44 +0800

    update class LoginPage

--- pages/login_page.py
+++ pages/login_page.py
@@ -15,12 +15,12 @@ class LoginPage(BasePage):
         # login_url = self.browser.find_element(*LoginPageLocators.LOGIN_URL)
         login_url = self.browser.current_url
         assert (login_url.endswith('login/')), 'LOGIN_URL не найден!'
-        # assert True, 'LOGIN_URL не найден!'
 
     def should_be_login_form(self):
         login_form = self.browser.find_element(*LoginPageLocators.LOGIN_FORM)
         assert True, 'LOGIN_FORM не найден!'
 
     def should_be_register_form(self):
-        register_form = self.browser.find_element(*LoginPageLocators.REGISTER_FORM)
+        register_form = self.browser.find_element(
+            *LoginPageLocators.REGISTER_FORM)
         assert True, 'REGISTER_FORM не найден!'
