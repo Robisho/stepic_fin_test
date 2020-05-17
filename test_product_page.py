59ff41438551b1bb3270af9c87ef7153d729b0d1
Robisho <bigsten@list.ru>
2020-05-17 21:48:14 +0800

    add mark.parametrize

---
+++ test_product_page.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python3
+
+# pytest -v -s test_foo.py
+
+from .pages.product_page import ProductPage
+from .pages.base_page import BasePage
+import time
+import pytest
+# from .pages.login_page import LoginPage
+
+
+@pytest.mark.parametrize('link', [
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+    pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfile),
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+    # link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_basket()
+    answer = BasePage(browser, link)
+    answer.solve_quiz_and_get_code()
+    # time.sleep(2)
+    page.should_be_add_item_to_basket()
