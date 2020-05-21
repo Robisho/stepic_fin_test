6892e26e20f9a2e36ad2a501b81f0271eb32d899
Robisho <bigsten@list.ru>
2020-05-21 20:34:09 +0800

    add test_guest_cant_see_product_in_basket_opened_from_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -6,6 +6,7 @@
 
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -22,8 +23,19 @@ def test_guest_can_go_to_login_page(browser):
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()  # Гость открывает главную страницу
+    basket_page = BasketPage(browser, link)
+    # Переходит в корзину по кнопке в шапке сайта
+    basket_page.should_be_go_to_basket_page()
+    # Ожидаем, что в корзине нет товаров
+    basket_page.should_not_be_items_in_basket()
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page.should_be_basket_empty()
