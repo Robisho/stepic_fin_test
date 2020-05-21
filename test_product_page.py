f325d725164a96143df40ea4256d5f24a830090f
Robisho <bigsten@list.ru>
2020-05-21 20:35:18 +0800

    add test_guest_cant_see_product_in_basket_opened_from_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -5,6 +5,7 @@
 
 from .pages.product_page import ProductPage
 from .pages.base_page import BasePage
+from .pages.basket_page import BasketPage
 import time
 import pytest
 # from .pages.login_page import LoginPage
@@ -83,10 +84,13 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    pass
-'''
-    Гость открывает страницу товара
-    Переходит в корзину по кнопке в шапке
-    Ожидаем, что в корзине нет товаров
-    Ожидаем, что есть текст о том что корзина пуста
-'''
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()  # Гость открывает страницу товара
+    basket_page = BasketPage(browser, link)
+    # Переходит в корзину по кнопке в шапке сайта
+    basket_page.should_be_go_to_basket_page()
+    # Ожидаем, что в корзине нет товаров
+    basket_page.should_not_be_items_in_basket()
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page.should_be_basket_empty()
