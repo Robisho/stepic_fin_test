04662a16f12d87070cb31cd429db84cf3ffbd4b0
Robisho <bigsten@list.ru>
2020-05-23 22:03:16 +0800

    update should_be_add_item_to_basket

--- pages/product_page.py
+++ pages/product_page.py
@@ -20,10 +20,10 @@ class ProductPage(BasePage):
             *ProductPageLocators.BASE_BOOKNAME).text
         base_price = self.browser.find_element(
             *ProductPageLocators.BASE_PRICE).text
-        add_name = self.browser.find_element_by_xpath(
-            '//*[@id="messages"]//strong').text
-        add_price = self.browser.find_element_by_xpath(
-            '//*[@id="messages"]//p[1]/strong').text
+        add_name = self.browser.find_element(
+            *ProductPageLocators.ADD_NAME).text
+        add_price = self.browser.find_element(
+            *ProductPageLocators.ADD_PRICE).text
 
         assert base_name == add_name, 'Добавлена другая книга!'
         assert base_price == add_price, 'Цена не соответствует!'
